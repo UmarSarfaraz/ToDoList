f0c3e248e4ec13b5609b040374f767de9f2d6824
UmarSarfaraz <89626401+UmarSarfaraz@users.noreply.github.com>
2021-09-01 22:00:39 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import  Ap from "./Ap";
+import "./todo.css";
+
+ReactDOM.render(
+    <>
+    <Ap/>
+    </>,
+    document.getElementById("root")
+);
